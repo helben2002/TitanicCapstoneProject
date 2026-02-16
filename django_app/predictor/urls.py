022ae0967a1d79ec0a9815cb0ea469fe5b759bc7
FurtiAdi <furtiadi@gmail.com>
2026-02-17 00:50:55 +0100

    data(TITANIC-81): resolve URL syntax error and update base template after rebase

--- django_app/predictor/urls.py
+++ django_app/predictor/urls.py
@@ -5,11 +5,8 @@ app_name = 'predictor'
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
     path('history/', views.history, name='history'),
-]
-=======
     path('predict/', views.predict_view, name='predict_view'), 
 ]
 
->>>>>>> e6c99aa (data(TITANIC-81): add Django URL routing for prediction view)
+
