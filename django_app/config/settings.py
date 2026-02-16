862606fba2f23a56bae6f0feb3cc3ff8c101fb16
FurtiAdi <furtiadi@gmail.com>
2026-02-16 10:44:02 +0100

    data(TITANIC-81): adjust BASE_DIR path and append to sys.path

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(BASE_DIR))
 
 
 # Quick-start development settings - unsuitable for production
