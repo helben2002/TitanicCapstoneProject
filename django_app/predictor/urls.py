b8d5b923526f21aff3cf2ed299ba218458e86f7f
FurtiAdi <furtiadi@gmail.com>
2026-02-16 10:29:26 +0100

    data(TITANIC-81): add Django URL routing for prediction view

--- django_app/predictor/urls.py
+++ django_app/predictor/urls.py
@@ -5,5 +5,11 @@ app_name = 'predictor'
 
 urlpatterns = [
     path('', views.home, name='home'),
+<<<<<<< HEAD
     path('history/', views.history, name='history'),
 ]
+=======
+    path('predict/', views.predict_view, name='predict_view'), 
+]
+
+>>>>>>> e6c99aa (data(TITANIC-81): add Django URL routing for prediction view)
