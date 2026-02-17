e786d8c775dc9d59ffaa2841e4827c8591ae3fea
FurtiAdi <furtiadi@gmail.com>
2026-02-17 10:05:04 +0100

    data(Titanic-82):Design database model to store input data, prediction, probability, and timestamp

--- django_app/predictor/models.py
+++ django_app/predictor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Prediction(models.Model):
+    name = models.CharField(max_length=255)
+
+    passenger_class = models.IntegerField()
+    sex = models.CharField(max_length=10)
+    age = models.FloatField()
+
+    sibling_spouse_count = models.IntegerField()
+    parent_child_count = models.IntegerField()
+
+    fare = models.FloatField()
+    embarked_port = models.CharField(max_length=1)
+
+    predicted_survival = models.BooleanField()
+    probability = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} → {self.predicted_survival}"
--- django_app/predictor/views.py
+++ django_app/predictor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import datetime
 
 from .forms import PassengerForm
+from .models import Prediction
 from ml.predict_service import predict_passenger
 
 
@@ -10,39 +11,7 @@ def home(request):
 
 
 def history(request):
-    # TODO: fetch predictions from database later
-
-    # Dummy data for testing
-    predictions = [
-        {
-            'id': 1,
-            'passenger_class': 'First Class',
-            'sex': 'Female',
-            'age': 22,
-            'sibling_spouse_count': 1,
-            'parent_child_count': 0,
-            'fare': 7.25,
-            'embarked_port': 'S',
-            'name': 'Heijkenskjöld, Miss Sara',
-            'predicted_survival': True,
-            'probability': 0.85,
-            'created_at': datetime.now()
-        },
-        {
-            'id': 2,
-            'passenger_class': 'Second Class',
-            'sex': 'Male',
-            'age': 35,
-            'sibling_spouse_count': 0,
-            'parent_child_count': 1,
-            'fare': 7.25,
-            'embarked_port': 'S',
-            'name': 'Heijkenskjöld, Mr. John',
-            'predicted_survival': False,
-            'probability': 0.80,
-            'created_at': datetime.now()
-        }
-    ]
+    predictions = Prediction.objects.all().order_by("-created_at")
 
     return render(request, 'predictor/history.html', {
         'predictions': predictions
@@ -58,6 +27,20 @@ def predict_view(request):
         if form.is_valid():
             data = form.cleaned_data
             result, probability = predict_passenger(data)
+
+            Prediction.objects.create(
+                name=data["Name"],
+                passenger_class=data["Pclass"],
+                sex=data["Sex"],
+                age=data["Age"],
+                sibling_spouse_count=data["SibSp"],
+                parent_child_count=data["Parch"],
+                fare=data["Fare"],
+                embarked_port=data["Embarked"],
+                predicted_survival=bool(result),
+                probability=probability
+            )
+
     else:
         form = PassengerForm()
 
